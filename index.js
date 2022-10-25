5a8d35e77a2365a22c84b65a5ed0f1797d446c6c
Esibe Vanessa <66547936+vani-gold@users.noreply.github.com>
2022-10-25 15:19:47 +0100

    remove port number to host my web app on Heroku
    remove the port number to host my web app on Heroku

--- index.js
+++ index.js
@@ -91,6 +91,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8001, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8001');
-});
+// server.listen(8001, '127.0.0.1', () => {
+//   console.log('Listening to requests on port 8001');
+// });
